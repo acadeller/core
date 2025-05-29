84d62ea8dd04250fd983248dbc20a4a40b0e77f9
Cesar Alvarez <cesaralv97@gmail.com>
2025-05-29 18:04:14 +0200

    refactor: clean up dependencies and improve project structure

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,47 +1,40 @@
-import mdx from "@astrojs/mdx";
+import { defineConfig } from "astro/config";
+import config from "./src/config/config.json";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import AutoImport from "astro-auto-import";
-import { defineConfig, squooshImageService } from "astro/config";
-import remarkCollapse from "remark-collapse";
-import remarkToc from "remark-toc";
-import config from "./src/config/config.json";
-
+import readingTime from "./src/lib/utils/readingTime.ts";
+import mdx from "@astrojs/mdx";
 import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
-  buildOptions: {
-    esbuild: {
-      define: {
-        __VUE_OPTIONS_API__: true,
-        __VUE_PROD_DEVTOOLS__: false,
-      },
-
-      alias: {
-        "@": "./src",
-        "@/*": "./src/*"
-      },
-    },
-  },
   site: config.site.base_url ? config.site.base_url : "http://examplesite.com",
   base: config.site.base_path ? config.site.base_path : "/",
   trailingSlash: config.site.trailing_slash ? "always" : "never",
-  image: {
-    service: squooshImageService()
-  },
-  integrations: [react(), sitemap(), tailwind({
-    config: {
-      applyBaseStyles: false
-    }
-  }), AutoImport({
-    imports: ["@/shortcodes/Button", "@/shortcodes/Accordion", "@/shortcodes/Notice", "@/shortcodes/Video", "@/shortcodes/Youtube", "@/shortcodes/Tabs", "@/shortcodes/Tab"]
-  }), mdx(), vue()],
+  integrations: [
+    react(), 
+    sitemap(), 
+    tailwind({
+      config: {
+        applyBaseStyles: false
+      }
+    }), 
+    AutoImport({
+      imports: [
+        "@/shortcodes/Button", 
+        "@/shortcodes/Accordion", 
+        "@/shortcodes/Notice", 
+        "@/shortcodes/Video", 
+        "@/shortcodes/Tabs", 
+        "@/shortcodes/Tab"
+      ]
+    }), 
+    mdx(), 
+    vue()
+  ],
   markdown: {
-    remarkPlugins: [remarkToc, [remarkCollapse, {
-      test: "Table of contents"
-    }]],
     shikiConfig: {
       theme: "one-dark-pro",
       wrap: true
@@ -50,5 +43,17 @@ export default defineConfig({
   },
   build: {
     format: "file"
+  },
+  vite: {
+    resolve: {
+      alias: {
+        "@/components": "/src/layouts/components",
+        "@/shortcodes": "/src/layouts/shortcodes",
+        "@/helpers": "/src/layouts/helpers",
+        "@/partials": "/src/layouts/partials",
+        "@/layouts": "/src/layouts",
+        "@": "/src"
+      }
+    }
   }
 });
